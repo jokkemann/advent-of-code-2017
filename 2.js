b053030f7600f97b6608e43b69c3e37f9aa20416
Joakim Svensson <svensson.joakim@gmail.com>
2017-12-02 11:46:28 +0100

    Add solution for day 2

---
+++ 2.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+//const input = ['5 1 9 5', '7 5 3', '2 4 6 8']
+//const input = ['5 9 2 8', '9 4 7 3', '3 8 6 5']
+
+const input = fs.readFileSync('./inputs/2.txt').toString().split('\n').filter(s => s !== '')
+
+function getChecksum(input) {
+	let sum = 0
+	let sum2 = 0
+	input.forEach((s) => {
+		let numbers = s.split(/\s/).map(num => parseInt(num))
+
+		numbers.sort((a, b) => b - a)
+
+		sum += numbers[0] - numbers[numbers.length - 1]
+
+		for (let i = 0; i < numbers.length; i++) {
+			let number = numbers[i]
+			let result = isEvenlyDivisible(number, numbers.slice(i+1))
+			if (result) {
+				sum2 += result
+				break;
+			}
+		}
+	})
+
+
+	return { task1: sum, task2: sum2 }
+}
+
+function isEvenlyDivisible(number, list) {
+	let divisor = list.find(n => number % n === 0)
+	return divisor ? number / divisor : 0
+}
+
+console.log(getChecksum(input))
