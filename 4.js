7398893d4c13f75a21364f0f7a3d56a61392ab07
Joakim Svensson <svensson.joakim@gmail.com>
2017-12-04 22:03:52 +0100

    Add day 4 task 2 solution

--- 4.js
+++ 4.js
@@ -1,12 +1,11 @@
 let fs = require('fs')
 
-//let input = ['aa bb cc dd ee', 'aa bb cc dd ee aa', 'aa bb cc dd ee aaa']
 let input = fs.readFileSync('./inputs/4.txt').toString().split('\n').filter(s => s !== '').map(s => s.trim())
-
 
 function numberOfValidPassPhrases(inputStrings) {
 	return inputStrings.reduce((acc, s) => {
 		let keys = {}
+
 		s.split(' ').forEach(w => {
 			if (keys[w] !== undefined) {
 				keys[w]++
@@ -18,4 +17,22 @@ function numberOfValidPassPhrases(inputStrings) {
 	}, 0)
 }
 
-console.log(numberOfValidPassPhrases(input))
+function numberOfValidPassPhrasesUsingAnagrams(inputStrings) {
+	return inputStrings.reduce((acc, s) => {
+
+		let keys = {}
+
+		s.split(' ').map(w => w.split('').sort().join('')).forEach(w => {
+			if (keys[w] !== undefined) {
+				keys[w]++
+			} else {
+				keys[w] = 0
+			}
+		})
+		return Object.keys(keys).every(k => keys[k] === 0) ? acc + 1 : acc
+	}, 0)
+}
+
+console.log('task 1:', numberOfValidPassPhrases(input))
+
+console.log('task 2:', numberOfValidPassPhrasesUsingAnagrams(input))
